8cd692040d0a4b6fbb6f7b2a0b3eb222c474f92e
Marc-Alban <millet.marcalban@gmail.com>
2019-10-08 19:21:59 +0200

    Menu créer et titre modifié avec organigrame

--- templates/frontend/headerView.php
+++ templates/frontend/headerView.php
@@ -1,1 +1,58 @@
-
+<div class="bgded overlay">
+    <div class="wrapper row1">
+        <header id="header" class="hoc clear">
+            <div id="logo" class="fl_left">
+                <h1><a href="index.php">UNC-Ecully</a></h1>
+            </div>
+            <nav id="mainav" class="fl_right">
+                <ul class="clear">
+                    <li class="active"><a href="index.php">Accueil</a></li>
+                    <li><a class="drop" href="#">Présentation</a>
+                        <ul>
+                            <li><a href="#">Fédération UNC</a></li>
+                            <li><a href="#">UNC départemental Rhône</a></li>
+                            <li><a href="#">UNC Ecully et ses environs</a></li>
+                            <li><a href="#">Horaires de permanence</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="drop" href="#">Actualité</a>
+                        <ul>
+                            <li><a href="#">Assemblées générales</a></li>
+                            <li><a href="#">Conseils d'Administration</a></li>
+                            <li><a href="#">Manifestations patriotiques</a></li>
+                            <li><a href="#">Evènements: culturels, sportifs, autres ... </a></li>
+                            <li><a href="#">Phototèque: classement par année</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="drop" href="#">Action Sociale</a>
+                        <ul>
+                            <li><a href="#">Missions</a></li>
+                            <li><a href="#">Code des pensions d'invalidité de guerre commenté</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="drop" href="#">Rejoignez-nous</a>
+                        <ul>
+                            <li><a href="#">Pourquoi nous rejoindre</a></li>
+                            <li><a href="#">Qui peut nous rejoindre</a></li>
+                            <li><a href="#">Bulletin d'adhésion</a></li>
+                            <li><a href="#">Coordonées du siege</a></li>
+                            <li><a href="#">Carte (plan)</a></li>
+                            <li><a href="#">Contact (nom/tel)</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+    <section id="pageintro" class="hoc clear">
+        <img src="img/logogauche.png" alt="">
+        <div>
+            <p><span class="heading ">UNC ECULLY</span></p>
+            <p><span class="heading  ">ET</span></p>
+            <p><span class="heading "> SES ENVIRONS</span> </p>
+            <p><span class="sizeMoy">COMBATTANTS</span></p>
+            <p> d'hier, d'aujourd'hui ... et de demain</p>
+        </div>
+        <img src="img/logodroite.png" alt="">
+    </section>
+</div>
