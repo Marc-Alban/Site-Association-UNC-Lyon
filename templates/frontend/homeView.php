1f1347c2dea44ff0b1002ee3eaf78c6e18b8895f
Marc-Alban <millet.marcalban@gmail.com>
2019-10-08 10:55:38 +0200

    site unc (organisation  du site)

--- templates/frontend/homeView.php
+++ templates/frontend/homeView.php
@@ -1,6 +1,1 @@
-<div class="container">
-    <div class="row">
-        <h1>Organigrame de l'association:</h1>
-        <img src="img/organigramme.png" alt="organigrame entreprise" class="organigramme">
-    </div>
-</div>
+
